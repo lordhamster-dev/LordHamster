c05c74c85a1405fa5537d70081bdc126d467342e
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-26 17:04:15 +0800

    solve some type issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,13 @@
-// Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
-// Define a schema for each collection you'd like to validate.
-const postsCollection = defineCollection({
+
+const posts = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string(),
-    date: z.date(),
+    date: z.coerce.date(),
     tags: z.array(z.string()),
     banner: z.string(),
   }),
 });
-// Export a single `collections` object to register your collection(s)
-export const collections = {
-  posts: postsCollection,
-};
+
+export const collections = { posts };
