9a1fb2e1cad0b412f5788a1a8589b198cb9ac728
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-26 16:48:15 +0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://lordhamster.com",
   integrations: [sitemap(), tailwind()],
 });
-
