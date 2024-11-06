c3219c0c2f3f265684ff5d695af4ff4ae79e13dc
lordhamster-dev <zhaojinbiao@lordhamster.com>
2024-11-06 18:07:12 +0800

    update

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -2,17 +2,11 @@ import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
 
 const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
-  return posts
-    .filter(postFilter)
-    .sort(
-      (a, b) =>
-        Math.floor(
-          new Date(b.data.modDatetime ?? b.data.pubDatetime).getTime() / 1000
-        ) -
-        Math.floor(
-          new Date(a.data.modDatetime ?? a.data.pubDatetime).getTime() / 1000
-        )
-    );
+	return posts
+		.filter(postFilter)
+		.sort(
+			(a, b) => b.data.pubDatetime.valueOf() - a.data.pubDatetime.valueOf(),
+		);
 };
 
 export default getSortedPosts;
