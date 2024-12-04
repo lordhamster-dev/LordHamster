bfc20d078cad9f30e968fe4c51c8c712de72b5cd
lordhamster-dev <zhaojinbiao@lordhamster.com>
2024-12-04 10:17:50 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import type { LicenseConfig, NavBarConfig, ProfileConfig, SiteConfig } from "./t
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-    title: "LordHamster's Blog",
-    subtitle: "Little Blog",
+    title: "请叫我仓鼠阁下",
+    subtitle: "",
     lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
     themeColor: {
         hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
